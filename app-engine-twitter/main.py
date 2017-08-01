851bc0d6a3d8dd48124b39c33bc6b80ef4711749
CSSI <css@CSSs-MacBook-Air.local>
2017-08-01 10:20:06 -0500

    :)

--- app-engine-twitter/main.py
+++ app-engine-twitter/main.py
@@ -15,11 +15,16 @@
 # limitations under the License.
 #
 import jinja2
+import os
 import webapp2
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        my_template = jinja_environment.get_template('templates/twitter_shade.html')
+        self.response.write(my_template.render())
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
