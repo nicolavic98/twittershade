d908d8e911911a1359597b3c34422c3f361de303
CSSI <css@CSSs-MacBook-Air.local>
2017-08-04 10:05:14 -0500

    responses

--- app-engine-twitter/responses.py
+++ app-engine-twitter/responses.py
@@ -5,4 +5,4 @@ below10k = ["TP someone's house", "Go to an abandoned warehouse", "Take a shower
 
 tenKto25K = ["Slap the person next to you", "Inhale Sour Patch Kids sugar", "Order 100 pizzas and send them to your neighbor's house", "Go To Walmart & yell loud on the speaker", "Take a shot of hotsauce"]
 
-above25K = ["Cut someone's hair while they're asleep", "Take a bucket to 7-Eleven, fill it up and leave without paying", "Ask a random dude on the bus if they want to be your daddy"]
+above25K = ["Cut someone's hair while they're asleep", "Take a bucket to 7-Eleven, fill it up and leave without paying", "Go to a public place and yell FIREEEEEE!!!!"]
